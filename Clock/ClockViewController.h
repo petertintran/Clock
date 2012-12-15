176009b87615e87c6111e922262770ef5078c0ba
petertintran <petertintran@gmail.com>
2012-12-14 22:29:38 -0800

    first commit

---
+++ Clock/ClockViewController.h
@@ -0,0 +1,13 @@
+//
+//  ClockViewController.h
+//  Clock
+//
+//  Created by Lion User on 14/12/2012.
+//  Copyright (c) 2012 Tran.Peter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClockViewController : NSObject
+
+@end
---
+++ Clock/ClockViewController.m
@@ -0,0 +1,13 @@
+//
+//  ClockViewController.m
+//  Clock
+//
+//  Created by Lion User on 14/12/2012.
+//  Copyright (c) 2012 Tran.Peter. All rights reserved.
+//
+
+#import "ClockViewController.h"
+
+@implementation ClockViewController
+
+@end
